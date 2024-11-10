173a9e2782556fb215da3d6b60fa2a184e2f0838
0anush0 <anushrohanishukla@gmail.com>
2024-11-09 21:56:05 -0600

    fix

--- build.js
+++ build.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
+const path = require('path');
 
+// Define the directory and file paths
+const distDir = path.join(__dirname, 'dist');
+const indexPath = path.join(distDir, 'index.html');
+
+// Create 'dist' directory if it doesn't exist
+if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+}
+
+// Read the original index.html file
 const content = fs.readFileSync('index.html', 'utf8');
+
+// Replace placeholder with actual API key
 const updatedContent = content.replace(/\$\{process\.env\.GOOGLE_MAPS_API_KEY\}/g, process.env.GOOGLE_MAPS_API_KEY);
-fs.writeFileSync('dist/index.html', updatedContent);
+
+// Write the updated content to 'dist/index.html'
+fs.writeFileSync(indexPath, updatedContent);
+
+console.log(`'${indexPath}' has been created successfully.`);
